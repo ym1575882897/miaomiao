36370f87850d01e8d4b474813f5eafe77813cadb
yangmeng <1575882897@qq.com>
2021-08-27 13:28:00 +0800

    第一次提交

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,59 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/cinema',
+    name: 'cinema',
+    component: () => import('@/views/Cinema')
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/mine',
+    name: 'mine',
+    component: () => import('@/views/Mine')
+  },
+  {
+    path: '/movie',
+    name: 'movie',
+    component: () => import('@/views/Movie'),
+    children: [
+      {
+        path: 'city',
+        component: () => import('@/components/City')
+      },
+      {
+        path: 'nowPlaying',
+        component: () => import('@/components/NowPlaying')
+      },
+      {
+        path: 'comingSoon',
+        component: () => import('@/components/ComingSoon')
+      },
+      {
+        path: 'search',
+        component: () => import('@/components/Search')
+      },
+      //   {
+      //     path: '/movie',
+      //     // 重定向
+      //     redirect: '/movie/nowPlaying',
+      //   }
+    ]
+  },
+  {
+    path: '/*',
+    redirect: '/movie'
+  },
 ]
 
 const router = new VueRouter({
-  routes
+  routes,
+  mode: 'history',
+  base: process.env.BASE_URL,
+
 })
 
 export default router
---
+++ src/views/Mine/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="main">
+    <Header title="我的喵喵" />
+    <div id="contenet">
+      <Login />
+    </div>
+    <TabBar />
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header";
+import TabBar from "@/components/TabBar";
+import Login from "@/components/Login";
+export default {
+  name: "Mine",
+  components: {
+    Header,
+    TabBar,
+    Login,
+  },
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<style  scoped>
+</style>
